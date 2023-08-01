5078da74746eceb5b276e7862e0aff1fc3b3c90e
Oana Istoc <o.istoc@outlook.com>
2023-08-01 16:06:07 +0300

    Sample App

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,11 @@
+import { Application } from "@hotwired/stimulus";
+import "@hotwired/turbo-rails";
+import "controllers";
+
+const application = Application.start();
+
+// Configure Stimulus development experience
+application.debug = false;
+window.Stimulus = application;
+
+export { application };
